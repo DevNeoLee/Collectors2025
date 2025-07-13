a7762c596c9e2c9c7d211b26987cffb4d56001db
Justin Lee <devneolee@gmail.com>
2025-07-13 03:27:19 -0700

    style navigation, logos into different theme

--- src/src/components/footer/footer.tsx
+++ src/src/components/footer/footer.tsx
@@ -1,81 +1,103 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FaGithub, FaLinkedin, FaEnvelope, FaHeart } from 'react-icons/fa';
 
 import footerStyle from './footer.module.scss';
 import logo from '../../logoBox.svg';
-import blueplane from '../../blueplane_icon.svg';
-import gitLogo from '../../git_icon.png';
-import linkedinLogo from '../../linkedin-logo.png';
 
 const Footer: React.FC = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
-    <div className={footerStyle.container}>
-      <div className={footerStyle.logoContainer}>   
-        <div className={footerStyle.logo}>
-          <Link to="/">
-            <img src={logo} alt="logo" />
-          </Link>
-          <small>&copy; 2021</small>
+    <footer className={footerStyle.footer}>
+      <div className={footerStyle.container}>
+        {/* Main Footer Content */}
+        <div className={footerStyle.mainContent}>
+          {/* Brand Section */}
+          <div className={footerStyle.brandSection}>
+            <div className={footerStyle.logo}>
+              <Link to="/">
+                <img src={logo} alt="Collectors App Logo" />
+              </Link>
+            </div>
+            <p className={footerStyle.tagline}>
+              Premium Movie & Media Collections for True Collectors
+            </p>
+            <div className={footerStyle.socialLinks}>
+              <a 
+                href="https://github.com/DevNeoLee/redux-ecommerce-store" 
+                target="_blank" 
+                rel="noopener noreferrer"
+                aria-label="GitHub"
+              >
+                <FaGithub />
+              </a>
+              <a 
+                href="https://www.linkedin.com/in/justinklee215/" 
+                target="_blank" 
+                rel="noopener noreferrer"
+                aria-label="LinkedIn"
+              >
+                <FaLinkedin />
+              </a>
+              <a 
+                href="mailto:contact@collectorsapp.com" 
+                aria-label="Email"
+              >
+                <FaEnvelope />
+              </a>
+            </div>
+          </div>
+
+          {/* Quick Links */}
+          <div className={footerStyle.linksSection}>
+            <div className={footerStyle.linkGroup}>
+              <h4>Shop</h4>
+              <ul>
+                <li><Link to="/shop/movie">Movies</Link></li>
+                <li><Link to="/shop/animation">Animation</Link></li>
+                <li><Link to="/shop/tvseries">TV Series</Link></li>
+                <li><Link to="/shop/rarecollection">Rare Collections</Link></li>
+              </ul>
+            </div>
+
+            <div className={footerStyle.linkGroup}>
+              <h4>Account</h4>
+              <ul>
+                <li><Link to="/login">Sign In</Link></li>
+                <li><Link to="/checkout">Checkout</Link></li>
+                <li><Link to="/">My Account</Link></li>
+                <li><Link to="/">Order History</Link></li>
+              </ul>
+            </div>
+
+            <div className={footerStyle.linkGroup}>
+              <h4>Support</h4>
+              <ul>
+                <li><Link to="/">Help Center</Link></li>
+                <li><Link to="/">Contact Us</Link></li>
+                <li><Link to="/">Shipping Info</Link></li>
+                <li><Link to="/">Returns</Link></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        {/* Bottom Bar */}
+        <div className={footerStyle.bottomBar}>
+          <div className={footerStyle.bottomContent}>
+            <p className={footerStyle.copyright}>
+              © {currentYear} Collectors App. All rights reserved.
+            </p>
+            <div className={footerStyle.legalLinks}>
+              <Link to="/">Privacy Policy</Link>
+              <Link to="/">Terms of Service</Link>
+              <Link to="/">Cookie Policy</Link>
+            </div>
+          </div>
         </div>
       </div>
-      <div className={footerStyle.listContainer}>
-        <div className={footerStyle.list}>
-          <h4>Features</h4>
-          <ul>
-            <li>
-              <Link to="/checkout" target="_blank">Shopping Cart</Link> with Redux
-            </li>
-            <li>
-              <div>Payment with <a href="https://www.stripe.com" target="_blank" rel="noopener noreferrer">Stripe</a></div>
-            </li>
-            <li>
-              <Link to="/checkout" target="_blank">Checkout Page</Link>
-            </li>
-            <li>
-              <div>Backend with <a href="https://www.firebase.com" target="_blank" rel="noopener noreferrer">Firebase</a></div>
-            </li>
-            <li>
-              <Link to="/login" target="_blank">Sign Up/Log In Page</Link>
-            </li>
-          </ul>
-        </div>
-        <div className={footerStyle.list}>
-          <h4>Resources</h4>
-          <ul>
-            <li>
-              <div>Icons by <a href="https://www.freepik.com" target="_blank" rel="noopener noreferrer" title="Freepik">Freepik</a></div>
-            </li>
-            <li>
-              <div>Favicon/Icons by <a href="https://thenounproject.com/" target="_blank" rel="noopener noreferrer" title="Freepik">Noun Project</a></div>
-            </li>
-            <li></li>
-          </ul>
-        </div>
-        <div className={footerStyle.list}>
-          <h4>About</h4>
-          <ul className={footerStyle.about}>
-            <li>
-              <a href="https://devneolee.github.io/justinklee/" target="_blank" rel="noopener noreferrer">
-                <span>© Justin K Lee </span>
-                <img src={blueplane} alt="" />
-              </a>
-            </li>
-            <li>
-              <a target="_blank" rel="noopener noreferrer" href="https://github.com/DevNeoLee/redux-ecommerce-store">
-                <span>GitHub</span>
-                <img src={gitLogo} alt="" />
-              </a>
-            </li>      								       
-            <li>
-              <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/justinklee215/">
-                <span>LinkedIn</span>
-                <img src={linkedinLogo} alt="" />
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
+    </footer>
   );
 };
 
